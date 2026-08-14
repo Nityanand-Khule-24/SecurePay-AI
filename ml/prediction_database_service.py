72ed80b9959ba3da6c308ce85dd220a5e232f353
Nityanand khule <khulenityanand5@gmail.com>
2026-08-14 23:15:17 +0530

    backend integration part completing

---
+++ ml/prediction_database_service.py
@@ -0,0 +1,32 @@
+from prediction import predict_fraud
+from database.fraud_prediction_service import insert_fraud_prediction
+
+def predict_and_store(transaction_id,transaction_features):
+    """
+    run the fraud model and store thr=e prediction in supabase...
+    """
+
+    # run ml prediction
+    result = predict_fraud(transaction_features)
+
+    #prepare database recored
+    prediction_data ={
+        "transaction_id": transaction_id,
+        "fraud_probability": result["fraud_probability"],
+        "prediction": result["prediction"],
+        "risk_level": result["risk_level"],
+        "model_version": "RandomForest-v1"
+    }
+
+    # store prediction in supabase
+    stored_prediction= insert_fraud_prediction(
+        prediction_data
+    )
+
+    return{
+        "transaction_id": transaction_id,
+        "prediction": result["prediction"],
+        "fraud_probability": result["fraud_probability"],
+        "risk_level": result["risk_level"],
+        "database_record": stored_prediction
+    }
